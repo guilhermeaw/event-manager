94fc49b1e6bce1b3dd3639261c63ac4625669960
GUILHERME AUGUSTO WEISSHEIMER <guilherme.w@acad.univates>
2022-04-13 19:26:21 -0300

    Ajusta requisições com body

--- packages/api-gateway/src/proxy.js
+++ packages/api-gateway/src/proxy.js
@@ -2,6 +2,20 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const setupProxies = (app, routes) => {
   routes.forEach(route => {
-    app.use(`/api/${route.url}`, createProxyMiddleware(route.proxy));
+    const restream = function(proxyReq, req, res, options) {
+      if (req.body) {
+          let bodyData = JSON.stringify(req.body);
+          // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+          proxyReq.setHeader('Content-Type','application/json');
+          proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+          // stream the content
+          proxyReq.write(bodyData)
+          proxyReq.end();
+      }
+    }
+
+    const proxy = createProxyMiddleware({ ...route.proxy, onProxyReq: restream });
+    
+    app.use(`/api/${route.url}`, proxy);
   });
 }
