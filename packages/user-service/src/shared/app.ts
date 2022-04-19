4611326adcdde6982ca8e8d779cda67246f43321
guilhermeaw <guilherme.w@universo.univates.br>
2022-04-19 20:08:55 -0300

    Adiciona cors

--- packages/user-service/src/shared/app.ts
+++ packages/user-service/src/shared/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -9,6 +10,7 @@ initializeDataSource();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors);
