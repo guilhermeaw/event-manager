8b7f2aaa0b65a819361fd906a7e9fd48108c5fda
guilhermeaw <guilherme.w@universo.univates.br>
2022-04-24 16:02:20 -0300

    Ajusta rotas certificado

--- packages/certificate-service/src/modules/certificates/routes/certificates.routes.ts
+++ packages/certificate-service/src/modules/certificates/routes/certificates.routes.ts
@@ -6,12 +6,12 @@ const certificatesRouter = Router();
 
 const certificatesController = new CertificatesController();
 
-certificatesRouter.post('/', certificatesController.create);
+certificatesRouter.post('/create', certificatesController.create);
+certificatesRouter.post('/validate/:hash', certificatesController.findByHash);
 
 certificatesRouter.get(
   '/user/:user_id/event/:event_id',
   certificatesController.index,
 );
-certificatesRouter.get('/hash/:hash', certificatesController.findByHash);
 
 export default certificatesRouter;
